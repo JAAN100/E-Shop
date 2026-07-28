51a8608755c4700700f344022798bfbbc553f270
Hassan jaan <hassan.jan.solo@gamil.com>
2026-07-28 06:03:33 +0500

    Login UI Completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import { Home, LoginPage, SignUp } from './Routes.js'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
-    App.jsx
-  </>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='log-in' element={<LoginPage />}></Route>
+      <Route path='sign-up' element={<SignUp />}></Route>
+    </Routes>
   )
 }
 
---
+++ Frontend/src/Routes.js
@@ -0,0 +1,9 @@
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+
+export {
+    LoginPage,
+    SignUp,
+    Home
+}
