8f56250a7f544b928e49c859c2ceb7c70adbf1be
Hassan jaan <hassan.jan.solo@gamil.com>
2026-08-08 11:22:23 +0500

    Product Page UI completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { HomePage, LoginPage, SignUpPage, Activation } from './Routes.js'
+import { HomePage, LoginPage, SignUpPage, Activation, ProductsPage } from './Routes.js'
 import { ToastContainer, Bounce } from 'react-toastify';
 import store from './redux/store.js';
 import ScrollToTop from './components/ScrollToTop.jsx'
@@ -15,6 +15,7 @@ function App() {
         <Route path='/' element={<HomePage />}></Route>
         <Route path='log-in' element={<LoginPage />}></Route>
         <Route path='sign-up' element={<SignUpPage />}></Route>
+        <Route path='products' element={<ProductsPage />}></Route>
         <Route path='activation/:activation_token' element={<Activation />}></Route>
       </Routes>
       <ToastContainer
--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -2,9 +2,11 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import HomePage from "./pages/HomePage";
 import Activation from "./pages/Activation";
+import ProductsPage from "./pages/ProductsPage";
 export {
     LoginPage,
     SignUpPage,
     HomePage,
+    ProductsPage,
     Activation 
 }
